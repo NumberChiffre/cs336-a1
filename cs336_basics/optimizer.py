42f557c5e52a2a71f1aa437a2cca867c870ef466
Numberchiffre <llebox@hotmail.com>
2025-05-19 22:16:33 -0400

    Add AdamW

---
+++ cs336_basics/optimizer.py
@@ -0,0 +1,54 @@
+from collections.abc import Callable
+import torch
+import math
+
+
+class SGD(torch.optim.Optimizer):
+    def __init__(self, params, lr: float = 1e-3):
+        if lr < 0:
+            raise ValueError(f"Invalid learning rate: {lr}")
+        defaults = {"lr": lr}
+        super().__init__(params, defaults)
+
+    def step(self, closure: Callable | None = None):
+        loss = None if closure is None else closure()
+        for group in self.param_groups:
+            lr = group["lr"]  # Get the learning rate.
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                state = self.state[p]  # Get state associated with p.
+                t = state.get("t", 0)  # Get iteration number from the state, or initial value.
+                grad = p.grad.data  # Get the gradient of loss with respect to p.
+                p.data -= lr / math.sqrt(t + 1) * grad  # Update weight tensor in-place.
+                state["t"] = t + 1  # Increment iteration number.
+        return loss
+
+
+class AdamW(torch.optim.Optimizer):
+    def __init__(self, params, lr: float, weight_decay: float, betas: tuple[float, float], eps: float):
+        defaults = {"lr": lr, "weight_decay": weight_decay, "betas": betas, "eps": eps}
+        super().__init__(params, defaults)
+
+    def step(self, closure: Callable | None = None):
+        loss = None if closure is None else closure()
+        for group in self.param_groups:
+            lr = group["lr"]
+            weight_decay = group["weight_decay"]
+            beta_1, beta_2 = group["betas"]
+            eps = group["eps"]
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                state = self.state[p]
+                grad = p.grad
+                t = state.get("t", 1)
+                m = state.get("m", torch.zeros_like(p.data))
+                v = state.get("v", torch.zeros_like(p.data))
+                state["m"] = beta_1 * m + (1 - beta_1) * grad
+                state["v"] = beta_2 * v + (1 - beta_2) * grad.pow(2)
+                lr_t = lr * (math.sqrt(1 - beta_2**t) / (1 - beta_1**t))
+                p.data -= lr_t * state["m"] / (torch.sqrt(state["v"]) + eps)
+                p.data -= lr * weight_decay * p.data
+                state["t"] = t + 1
+        return loss
--- tests/adapters.py
+++ tests/adapters.py
@@ -12,6 +12,8 @@ from torch import Tensor
 
 from cs336_basics.train_bpe import train_bpe
 import cs336_basics.model as model
+import cs336_basics.loss as loss
+import cs336_basics.optimizer as optimizer
 
 
 def run_linear(
@@ -523,7 +525,7 @@ def get_adamw_cls() -> type[torch.optim.Optimizer]:
     """
     Returns a torch.optim.Optimizer that implements AdamW.
     """
-    raise NotImplementedError
+    return optimizer.AdamW
 
 
 def run_get_lr_cosine_schedule(
